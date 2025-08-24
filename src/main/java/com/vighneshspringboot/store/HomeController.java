bd151d70b7e59de062c72dd9c9455308c8aa1794
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2025-08-24 04:06:31 -0700

    Added Index.html in resources with request mapping for '/' to index.html

--- src/main/java/com/vighneshspringboot/store/HomeController.java
+++ src/main/java/com/vighneshspringboot/store/HomeController.java
@@ -1,7 +1,14 @@
 package com.vighneshspringboot.store;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @RequestMapping
+    public String index(){
+        return "index.html";
+    }
+
 }
+
