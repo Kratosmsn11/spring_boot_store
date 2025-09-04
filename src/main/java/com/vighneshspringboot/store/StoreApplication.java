d14da0629bd7601f542920503c7e73a29619ffb7
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2025-09-03 23:38:36 -0700

    Initial Commit

--- src/main/java/com/vighneshspringboot/store/StoreApplication.java
+++ src/main/java/com/vighneshspringboot/store/StoreApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(StoreApplication.class, args);
-        var orderService = new OrderService(new PayPalPaymentService());
-        orderService.placeOrder();
+		SpringApplication.run(StoreApplication.class, args);
+//        var orderService = new OrderService(new PayPalPaymentService());
+//        orderService.placeOrder();
 	}
 }
